0678e3316919c31ece115e2438eaebf8797016c7
Mino <0301mh@naver.com>
2023-09-15 02:36:44 +0900

    Add binary_serach.py

---
+++ SourceCode/search/binary_serach.py
@@ -0,0 +1,11 @@
+# 찾으려는 데이터와 중간점 위치에 있는 데이터를 반복적으로 비교
+def binary_search(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end)//2
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search(array, target, start, mid-1)
+    else:
+        return binary_search(array, target, mid+1, end)
