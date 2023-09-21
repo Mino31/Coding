1a86996435abcf7218dfd177c70fe882b900b513
Mino <0301mh@naver.com>
2023-09-22 00:24:27 +0900

    Add default_grammar1.py

---
+++ SourceCode/grammar/default_grammar1.py
@@ -0,0 +1,34 @@
+# integer
+a = 1000
+print(a)
+
+# Real number
+b = 1.5
+print(b)
+
+c = 1e5
+print(c)
+
+d = -.7
+print(d)
+
+# list
+e = [1, 2, 3, 4, 5]
+print(e[1])
+print(e[-1])
+print(e[1:3])
+
+f = list()
+g = []
+
+h = [i for i in range(10)]  # list comprehension
+h.append(11)
+h.sort()
+h.reverse()
+h.insert()
+h.count()
+h.remove()
+
+
+
+
